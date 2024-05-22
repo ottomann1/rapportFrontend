032e2ebf4e49cfcca6dba8b677f19a735727f5bd
ottomann1 <otto.kostmann@hotmail.com>
2024-05-22 12:51:59 +0200

    axios

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:8000', // Replace with your backend URL
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+axiosInstance.interceptors.request.use(
+    config => {
+      // Do something before request is sent, like adding authentication token
+      // config.headers.Authorization = `Bearer ${yourToken}`;
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  
+  axiosInstance.interceptors.response.use(
+    response => response,
+    error => {
+      // Handle response error
+      return Promise.reject(error);
+    }
+  );
+  
+  export default axiosInstance;
