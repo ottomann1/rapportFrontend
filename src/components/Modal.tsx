fe1d4ac83eb366c86d02339ca6aec7d9482792d2
ottomann1 <otto.kostmann@hotmail.com>
2024-05-23 13:47:04 +0200

    modal now closes when clicking outside of it

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Modal.css';
 
 interface ModalProps {
@@ -9,12 +9,31 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ show, onClose, title, children }) => {
+  const handleClickOutside = (event: MouseEvent) => {
+    const modalContent = document.getElementById('modal-content');
+    if (modalContent && !modalContent.contains(event.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (show) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [show]);
+
   if (!show) return null;
 
   return (
     <div className="modal fade show d-block" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content" id="modal-content">
           <div className="modal-header">
             <h5 className="modal-title">{title}</h5>
           </div>
