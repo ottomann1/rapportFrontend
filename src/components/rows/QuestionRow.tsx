665112086c642364b006deaa4333867e39bd6569
ottomann1 <otto.kostmann@hotmail.com>
2024-05-23 12:30:04 +0200

    questionrow

---
+++ src/components/rows/QuestionRow.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Question } from '../../types/models';
+import './QuestionRow.css';
+
+interface QuestionRowProps {
+  question: Question;
+}
+
+const calculateAnswers = (question: Question) => {
+  const answerCounts: { [key: string]: number } = { yes: 0, no: 0, other: 0 };
+
+  question.answers.forEach(answer => {
+    if (answer.answer === 'yes') answerCounts.yes += 1;
+    else if (answer.answer === 'no') answerCounts.no += 1;
+    else answerCounts.other += 1;
+  });
+
+  return answerCounts;
+};
+
+const QuestionRow: React.FC<QuestionRowProps> = ({ question }) => {
+  const answerCounts = calculateAnswers(question);
+
+  return (
+    <li className="question-item">
+      <span>{question.text}</span>
+      <span>Yes: {answerCounts.yes}</span>
+      <span>No: {answerCounts.no}</span>
+      <span>Other: {answerCounts.other}</span>
+    </li>
+  );
+};
+
+export default QuestionRow;
