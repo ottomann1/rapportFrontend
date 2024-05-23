4adba8075ced3a69a63ab523073166a9814a9e6e
ottomann1 <otto.kostmann@hotmail.com>
2024-05-23 11:44:20 +0200

    reporttemplatelist in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 // src/App.tsx
 
 import React from 'react';
-import CompanyList from './components/CompanyList';
-import ReportList from './components/ReportList';
+import CompanyList from './components/lists/CompanyList';
+import ReportList from './components/lists/ReportList';
+import ReportTemplateList from './components/lists/ReportTemplateList';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <ReportList />
+      <ReportTemplateList />
     </div>
   );
 };
