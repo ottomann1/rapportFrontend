de81b91b6d2ea277f93dd56308ddda8f5ae05685
ottomann1 <otto.kostmann@hotmail.com>
2024-05-23 12:34:09 +0200

    removed reportlist from main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// src/App.tsx
-
 import React from 'react';
 import CompanyList from './components/lists/CompanyList';
 import ReportList from './components/lists/ReportList';
@@ -9,7 +7,6 @@ const App: React.FC = () => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
-      <ReportList />
       <ReportTemplateList />
     </div>
   );
