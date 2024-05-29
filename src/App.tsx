3eef9412676455203a45914dafdef10a92afec8e
ottomann1 <otto.kostmann@hotmail.com>
2024-05-29 16:45:06 +0200

    this is not really doing what i want it to but im too tired to solve it right now

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import CustomNavbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider, useAuth } from './contexts/AuthContext';
 import PrivateRoute from './routes/PrivateRoute';
 
-const App: React.FC = () => {
-  // Sample user and company information
-  const userName = "John Doe";
-  const companyName = "Acme Corp";
+const DynamicNavbar: React.FC = () => {
+  const location = useLocation();
+  const { user } = useAuth();
 
+  const getPageName = (path: string) => {
+    switch (path) {
+      case '/':
+        return 'Home';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <CustomNavbar
+      pageName={getPageName(location.pathname)}
+      userName={user?.username ?? null}
+      companyName={user?.company ?? null}
+    />
+  );
+};
+
+const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
-        <CustomNavbar pageName="My App" userName={userName} companyName={companyName} />
+        <DynamicNavbar />
         <div className="container mt-5">
           <Routes>
             <Route path="/login" element={<LoginPage />} />
