80f38559faf1b5f8f3c06e88da474466ad109bd4
ottomann1 <otto.kostmann@hotmail.com>
2024-05-29 16:15:48 +0200

    axiosinstance

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,28 +1,52 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // Replace with your backend URL
+  baseURL: 'http://localhost:8000/api/',
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
+    accept: 'application/json',
   },
 });
+
 axiosInstance.interceptors.request.use(
-    config => {
-      // Do something before request is sent, like adding authentication token
-      // config.headers.Authorization = `Bearer ${yourToken}`;
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  );
-  
-  axiosInstance.interceptors.response.use(
-    response => response,
-    error => {
-      // Handle response error
-      return Promise.reject(error);
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && originalRequest.url === 'http://localhost:8000/api/token/refresh/') {
+      const refreshToken = localStorage.getItem('refreshToken');
+      return axiosInstance
+        .post('/token/refresh/', { refresh: refreshToken })
+        .then(response => {
+          localStorage.setItem('token', response.data.access);
+          axiosInstance.defaults.headers['Authorization'] = 'Bearer ' + response.data.access;
+          originalRequest.headers['Authorization'] = 'Bearer ' + response.data.access;
+          return axiosInstance(originalRequest);
+        })
+        .catch(err => {
+          console.error('Refresh token error', err);
+          window.location.href = '/login';
+          return Promise.reject(err);
+        });
     }
-  );
-  
-  export default axiosInstance;
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
