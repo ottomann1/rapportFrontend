fc9a79834e00e5321ba626cc0353dcb4fb998676
ottomann1 <otto.kostmann@hotmail.com>
2024-05-29 15:14:48 +0200

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import CompanyList from './components/lists/CompanyList';
-import ReportList from './components/lists/ReportList';
-import ReportTemplateList from './components/lists/ReportTemplateList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CustomNavbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './routes/PrivateRoute';
 
 const App: React.FC = () => {
+  // Sample user and company information
+  const userName = "John Doe";
+  const companyName = "Acme Corp";
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <ReportTemplateList />
-    </div>
+    <AuthProvider>
+      <Router>
+        <CustomNavbar pageName="My App" userName={userName} companyName={companyName} />
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<PrivateRoute element={<HomePage />} />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 
