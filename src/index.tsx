abdcdc99f452ea4e26ac10cb751a04c21acae41b
ottomann1 <otto.kostmann@hotmail.com>
2024-05-29 16:05:49 +0200

    new root instead react dom render

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
