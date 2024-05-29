cd6c34e235bd2708f227f5e192b5b5c03b542cac
ottomann1 <otto.kostmann@hotmail.com>
2024-05-29 15:13:55 +0200

    Added a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+interface NavbarProps {
+  pageName: string;
+  userName: string;
+  companyName: string;
+}
+
+const CustomNavbar: React.FC<NavbarProps> = ({ pageName, userName, companyName }) => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">{pageName}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item className="text-light mr-3">{userName}</Nav.Item>
+          <Nav.Item className="text-light">{companyName}</Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default CustomNavbar;
